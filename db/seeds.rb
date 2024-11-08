d31d581298876719dcca002f4d1eb7307b66a173
HabiDev <habidev@mail.ru>
2024-11-08 11:23:27 +0300

    Fix deploy

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+user = User.find_by_type_role('admin')
+
+unless user.present?
+  user_new = User.create(email: "fd@mail.ru", type_role: "admin", password: "Kzn2021$")
+  user_new.build_profile(full_name: 'Администратор', mobile: '+79272000000')
+end
