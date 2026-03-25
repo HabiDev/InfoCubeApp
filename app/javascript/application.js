5a5e6f37a4cf3d7030f5a6a572452e311e2a7faf
Фанис Хабибуллин <userfan@MacBook-Air-Fanis.local>
2026-03-25 16:37:13 +0300

    Fix Import

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -2,3 +2,12 @@
 import "@hotwired/turbo-rails"
 import "./controllers"
 import * as bootstrap from "bootstrap"
+
+document.addEventListener("submit", function(e) {
+  const form = e.target
+  const btn = form.querySelector('input[type="submit"], button[type="submit"]')
+  if (btn) {
+    btn.value = 'Подождите идет загрузка...'
+    btn.disabled = true
+  }
+})
